1f4608a3a6f9ce16e9e9efe86e2a9c8aa0d2dc68
Bastian Alegría <bastian.alegria@softtek.com>
2019-09-02 21:21:01 -0400

    1st commit

---
+++ src/main/java/com/rpa/ppmtickethelperservice/PpmTicketHelperServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rpa.ppmtickethelperservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmTicketHelperServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmTicketHelperServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rpa/ppmtickethelperservice/PpmTicketHelperServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rpa.ppmtickethelperservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PpmTicketHelperServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
